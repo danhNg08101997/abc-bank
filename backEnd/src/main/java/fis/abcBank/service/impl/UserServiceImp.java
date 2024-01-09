ccd61102dcfdf073f90fcf71bfa9f74a0d397005
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-01-09 14:17:49 +0700

    update ABC_User

---
+++ backEnd/src/main/java/fis/abcBank/service/impl/UserServiceImp.java
@@ -0,0 +1,12 @@
+package fis.abcBank.service.impl;
+
+import fis.abcBank.dto.request.UserRequest;
+import fis.abcBank.dto.response.BaseResponse;
+import fis.abcBank.service.UserService;
+
+public class UserServiceImp implements UserService {
+    @Override
+    public BaseResponse createUser(UserRequest request) {
+        return null;
+    }
+}
