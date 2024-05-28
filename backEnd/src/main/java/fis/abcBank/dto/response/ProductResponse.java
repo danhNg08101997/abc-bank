59e0ecf069f86e3709e30f703a6d11495f3177bd
danhnt43 <danhnt43@fpt.com>
2024-05-28 17:53:25 +0700

    update ProductResponse

--- backEnd/src/main/java/fis/abcBank/dto/response/ProductResponse.java
+++ backEnd/src/main/java/fis/abcBank/dto/response/ProductResponse.java
@@ -3,6 +3,9 @@ package fis.abcBank.dto.response;
 public class ProductResponse {
     private int productID;
     private String productName;
+    private String productCode;
+    private String productCreateDate;
+    private String productUpdateDate;
 
     public int getProductID() {
         return productID;
@@ -18,5 +21,29 @@ public class ProductResponse {
 
     public void setProductName(String productName) {
         this.productName = productName;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductCreateDate() {
+        return productCreateDate;
+    }
+
+    public void setProductCreateDate(String productCreateDate) {
+        this.productCreateDate = productCreateDate;
+    }
+
+    public String getProductUpdateDate() {
+        return productUpdateDate;
+    }
+
+    public void setProductUpdateDate(String productUpdateDate) {
+        this.productUpdateDate = productUpdateDate;
     }
 }
